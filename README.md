7f2dc67e667c471c6907ad9e891053558ec56560
Julian Bierbaum <bierbaumjulian@gmail.com>
2025-07-21 08:19:16 +0200

    fixed build bugs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # license-plate-system
 License plate recognition system for Zotter Schokoladen GmbH
+
+### Setup
+- run the `build.sh` file to build images and push them to public registry
+- run `docker compose up` to start the development stack
--- build.sh
+++ build.sh
@@ -1,29 +1,23 @@
 cd services/analytics-service
 
+uv sync
 docker build -t julianbierbaum/license-plate-system:analytics-service .
-
-wait 1
-
 docker push julianbierbaum/license-plate-system:analytics-service
 
 cd ..
 
 cd auth-service
 
+uv sync
 docker build -t julianbierbaum/license-plate-system:auth-service .
-
-wait 1
-
 docker push julianbierbaum/license-plate-system:auth-service
 
 cd ..
 
 cd data-collection-service
 
+uv sync
 docker build -t julianbierbaum/license-plate-system:data-collection-service .
-
-wait 1
-
 docker push julianbierbaum/license-plate-system:data-collection-service
 
 cd ..
@@ -31,19 +25,15 @@ cd ..
 cd notification-service
 
 docker build -t julianbierbaum/license-plate-system:notification-service .
-
-wait 1
-
+uv sync
 docker push julianbierbaum/license-plate-system:notification-service
 
 cd ..
 
 cd web-service
 
+npm install
 docker build -t julianbierbaum/license-plate-system:web-service .
-
-wait 1
-
 docker push julianbierbaum/license-plate-system:web-service
 
 cd ..
