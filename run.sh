8817a7a47204d88e2b5790ba79e75f5507d4580a
Julian Bierbaum <bierbaumjulian@gmail.com>
2025-07-24 12:49:53 +0200

    Update file permissions for build.sh and run.sh; standardize docker-compose commands

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 # Function to be executed on exit
 cleanup() {
     echo "Stopping services and removing volumes..."
-    docker-compose down
+    docker compose down
 }
 
 # Trap the EXIT signal and execute the cleanup function
 trap cleanup EXIT
 
 # Start the services
-docker-compose up
+docker compose up
